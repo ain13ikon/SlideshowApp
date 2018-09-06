503758b870ebf6f7d7bd88c63b61270aba35a617
ain <ain.git13@gmail.com>
2018-09-06 09:52:15 +0900

    再生・停止の表示切り替えを追加。

--- SlideshowApp/ViewController.swift
+++ SlideshowApp/ViewController.swift
@@ -9,10 +9,37 @@
 import UIKit
 
 class ViewController: UIViewController {
+    var playing_Flag: Bool! = false
+    
+    @IBOutlet weak var playStopButton: UIButton!
+    
+    func changePlayStop(){
+        if self.playing_Flag {
+            playStopButton.setTitle("再生", for: .normal)
+            
+        }else{
+            playStopButton.setTitle("停止", for: .normal)
+        }
+        self.playing_Flag = !self.playing_Flag
+    }
 
+    @IBAction func playStop(_ sender: UIButton) {
+        changePlayStop()
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        /*
+        self.playing_Flag = false
+        playStopButton.setTitle("再生", for: .normal)
+        if self.playing_Flag {
+            print("デバッグ　\(playing_Flag)")
+        }else{
+            print("デバッグ　\(playing_Flag)")
+        }
+         */
     }
 
     override func didReceiveMemoryWarning() {
